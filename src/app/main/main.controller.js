df309e3d2c247655ccec579e9c9e3c9d4b44475f
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2016-09-01 11:13:48 +0200

    Added back components

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dockerCouchAdmin')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($timeout) {
+    var vm = this;
+
+    vm.awesomeThings = [];
+    vm.classAnimation = '';
+    vm.creationDate = 1452170777550;
+
+    this.activate = function() {
+        $timeout(function() {
+          vm.classAnimation = 'rubberBand';
+        }, 4000);
+      };
+  }
+
+})();
